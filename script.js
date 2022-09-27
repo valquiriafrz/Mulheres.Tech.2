a38772d65ed47de9d6a38eb30725f548e06bebdf
valquiriafrz <vallmachado0047@gmail.com>
2022-09-27 16:52:40 -0300

    vfjn

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
 function runApp() {
 
   // Carrega a página inicial do site quando este iniciar:
-  loadPage('home');
+  loadPage('contacts');
 
   /**
    * jQuery → Quando houver click em um elemento <a>, execute o aplicativo 
