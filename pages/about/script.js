6e0cb61abe6f11b7ecbdf976e0b6b119935b3156
valquiriafrz <vallmachado0047@gmail.com>
2022-09-28 16:51:04 -0300

    SVCVSDF

--- pages/about/script.js
+++ pages/about/script.js
@@ -1,1 +1,14 @@
+/**
+ * JavaScript desta página.
+ */
+
+// Define o <title> desta página:
 setTitle("Sobre...");
+
+// Detecta clique nos cads
+$('.card').click(getHref)
+
+// Processa clique nos cads
+|function getHref() {
+    loadPage ($(this) .attr('data-href'))
+}
